04e9ac87d88dd5ec962024c1e10319ee2c515713
Jason Chung <jason.chung@skylinetw.com>
2021-02-25 19:43:26 +0800

    init project

---
+++ README.md
@@ -0,0 +1,17 @@
+# @0y0/gitbook-cli · [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/o0y0o/gitbook-cli/blob/master/LICENSE) [![npm](https://img.shields.io/npm/v/@0y0/gitbook-cli.svg)](https://www.npmjs.com/package/@0y0/gitbook-cli) ![Package Status](https://github.com/o0y0o/gitbook-cli/workflows/Package/badge.svg)
+
+`@0y0/gitbook-cli` is a Gitbook CLI.
+
+## Installation
+
+```
+npm install @0y0/gitbook-cli --global
+```
+
+## Usage
+
+Use `gitbook help` command to print the usage.
+
+## License
+
+[MIT](https://github.com/o0y0o/gitbook-cli/blob/master/LICENSE)
---
+++ bin/gitbook.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+const { commands } = require('gitbook')
+const yargs = require('yargs')
+
+for (const cmd of commands) {
+  const argKeys = cmd.name
+    .match(/\[[^\]]+\]/g)
+    .map(key => key.replace(/^\[(.+)\]$/, '$1'))
+  yargs.command(
+    cmd.name,
+    cmd.description,
+    yargs => {
+      for (const opt of cmd.options) {
+        yargs.option(opt.name, {
+          describe: opt.description,
+          choices: opt.values,
+          default: opt.defaults
+        })
+      }
+    },
+    argv => {
+      const { _, $0, ...kwargs } = argv
+      const args = argKeys
+        .map(key => kwargs[key])
+        .filter(value => value != null)
+      cmd.exec(args, kwargs)
+    }
+  )
+}
+yargs.strict(true).help().argv
